67056381c8745c3eccbe7c01ac544381bf7bc963
Sara Sissy <i.am.sara.the.sissy@gmail.com>
2024-05-11 17:05:52 +0200

    remove lfs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,11 +14,7 @@
               width="800"
               height="600"
               frameborder="0"
-              allow="accelerometer; ambient-light-sensor; autoplay; battery; camera; display-capture; encrypted-media; execution-while-not-rendered; execution-while-out-of-viewport; fullscreen; geolocation; gyroscope; layout-animations; legacy-image-formats; magnetometer; microphone; midi; navigation-override; oversized-images; payment; picture-in-picture; publickey-credentials-get; sync-xhr; usb; vr; wake-lock; web-share; xr-spatial-tracking"
               allowfullscreen
-              allowpaymentrequest
-              referrerpolicy="no-referrer"
-              sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation allow-top-navigation-by-user-activation"
             >
             </iframe>
           </v-card-text>
